89fae7029d9877bb88d4ffeaeea33f4fe734b876
Jonas Bulik <jonas@bulik.dev>
2024-01-19 16:07:00 +0100

    Fix melt-ui processing

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import { vitePreprocess } from '@astrojs/svelte';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
 
 export default {
-	preprocess: vitePreprocess(),
+	preprocess: sequence([vitePreprocess({}), preprocessMeltUI()]),
 };
