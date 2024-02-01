ed1cc46cc89a597a7a299d785000c934d1e162c4
Jonas Bulik <jonas@bulik.dev>
2024-02-01 13:22:00 +0100

    Fix img attributs in Calendar

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -22,7 +22,8 @@
 			{#each events as event, index}
 				<img
 					loading="lazy"
-					{...event.image.src}
+					{...event.image.src.attributes}
+					src={event.image.src.src}
 					alt="Leute sitzen am Tisch"
 					class={clsx(
 						'absolute left-0 top-0 h-full w-full rounded-t-2xl object-cover transition-opacity duration-300 lg:rounded-l-2xl lg:rounded-tr-none',
