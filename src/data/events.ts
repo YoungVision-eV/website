a70b7d00aa278b90d0ad3c896d23f9c7792871a3
Jonas Bulik <jonas@bulik.dev>
2024-02-02 11:31:22 +0100

    Clean up events data

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,8 +1,5 @@
 import { getImage } from 'astro:assets';
 
-import EventImage1 from '@assets/events/projects-event-image-1.jpeg';
-import EventImage2 from '@assets/events/projects-event-image-2.jpeg';
-import EventImage3 from '@assets/events/projects-event-image-3.jpeg';
 import pastEvent from '@assets/events/calendar-past-event.jpeg';
 import thirdEventImage from '@assets/events/calendar-third-event.jpeg';
 import calendarCoverImage from '@assets/events/calendar-cover.jpeg';
@@ -48,55 +45,4 @@ export async function getNext3Events(): Promise<Event[]> {
 		},
 	];
 	return next3Events;
-}
-
-type YVEvent = {
-	slug: string;
-	title: string;
-	day: string;
-	month: string;
-	short_description: string;
-	image: string;
-	for_all: boolean;
-	future?: string;
-};
-
-const yearlyEvents: YVEvent[] = [
-	{
-		slug: 'summer-gathering',
-		title: 'Sommer Gathering',
-		day: '2',
-		month: 'September',
-		short_description:
-			'Unsere jährliche Sommerveranstaltung ist für viele das Highlight des Jahres!',
-		image: EventImage1.src,
-		for_all: true,
-	},
-	{
-		slug: 'silvester',
-		title: 'Silvester',
-		day: '31',
-		month: 'Dezember',
-		short_description:
-			'Lass uns das vergangene Jahr ausklingen lassen und gemeinsam in das neue Jahr starten!',
-		image: EventImage2.src,
-		for_all: true,
-	},
-	{
-		slug: 'mitgliederversammlung',
-		title: 'Mitgliederversammlung',
-		day: '4',
-		month: 'April',
-		short_description: 'Werde Mitglied und entscheide gemeinsam über die Zukunft von YoungVision!',
-		image: EventImage3.src,
-		for_all: false,
-	},
-];
-
-export function getEventBySlug(slug: string) {
-	return yearlyEvents.find((e) => e.slug === slug);
-}
-
-export function getAllYearlyEvents() {
-	return yearlyEvents;
 }
