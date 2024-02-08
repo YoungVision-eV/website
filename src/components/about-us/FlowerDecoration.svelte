7379aaf7006f51f3f66fa8e0a00427fc6f6d6d5c
Han2-Ro <hannes.rother@online.de>
2024-02-08 13:27:15 +0100

    added TryThisAtHome

---
+++ src/components/about-us/FlowerDecoration.svelte
@@ -0,0 +1,11 @@
+<script>
+	import FlowerExplosion from '@assets/about-us/flower-explosion.png';
+</script>
+
+<div class="w-full overflow-hidden">
+	<img
+		src={FlowerExplosion.src}
+		alt="Flower explosion"
+		class="h-60 w-full object-cover object-top lg:h-72"
+	/>
+</div>
