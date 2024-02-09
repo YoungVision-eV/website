398842b8448cd3ecba1372625f98350ad74896d4
Han2-Ro <hannes.rother@online.de>
2024-02-09 13:56:09 +0100

    fix eslint

--- tests/about-us.spec.ts
+++ tests/about-us.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
-import exp from 'constants';
 
 test.beforeEach(async ({ page }) => {
 	await page.goto('/about-us');
 });
 
 test('About Us page screenshot', async ({ page }) => {
-	let portraitEmma = page.getByAltText('Emma');
+	const portraitEmma = page.getByAltText('Emma');
 	portraitEmma.scrollIntoViewIfNeeded();
 	expect(portraitEmma).toBeVisible();
 	await page.waitForTimeout(1000);
