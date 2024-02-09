ee25a3303b707ede2e385843ed59095ef39f0876
Han2-Ro <hannes.rother@online.de>
2024-02-09 13:53:28 +0100

    fix test

--- tests/about-us.spec.ts
+++ tests/about-us.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 
 test.beforeEach(async ({ page }) => {
 	await page.goto('/about-us');
 });
 
 test('About Us page screenshot', async ({ page }) => {
+	let portraitEmma = page.getByAltText('Emma');
+	portraitEmma.scrollIntoViewIfNeeded();
+	expect(portraitEmma).toBeVisible();
+	await page.waitForTimeout(1000);
 	await page.getByRole('contentinfo').scrollIntoViewIfNeeded();
 	await expect(page).toHaveScreenshot({ fullPage: true });
 });
