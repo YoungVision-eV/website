31eac000c58a113c4cc981ab8dd39a582ee39ea4
Jonas Bulik <jonas@bulik.dev>
2024-02-10 15:40:40 +0100

    test(tools): 21 fix missing html reports in CI
    With 2c4ec72b2 the html report was not uploaded anymore.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,20 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig, ReporterDescription } from '@playwright/test';
 import { devices } from '@playwright/test';
 
 const webServer = process.env.PLAYWRIGHT_TEST_BASE_URL
 	? undefined
 	: {
-			command: 'pnpm run build && pnpm run preview',
-			port: 4321,
-		};
+		command: 'pnpm run build && pnpm run preview',
+		port: 4321,
+	};
+
+const reporter: ReporterDescription[] = process.env.CI
+	? [
+		['github', { outputMode: 'summary' }],
+		['html', { outputFolder: 'playwright-report', open: 'never' }],
+		['junit', { outputFile: 'playwright-results.xml' }],
+	]
+	: [['list']];
 
 const config: PlaywrightTestConfig = {
 	retries: 3,
@@ -38,7 +46,7 @@ const config: PlaywrightTestConfig = {
 	],
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-	reporter: [[process.env.CI ? 'github' : 'list'], ['html']],
+	reporter,
 };
 
 export default config;
