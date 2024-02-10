654ff29e92563a9a55c866fb93daca157add0208
Jonas <jonas@bulik.dev>
2024-02-10 15:57:38 +0100

    Mrgreentea/feat/port bauwoche event page (#32)
    * feat(app): port members page
    Closes https://github.com/YoungVision-eV/homepage-sveltekit/issues/220
    
    * feat(app): small text improvements
    less repetition, some more clarity

---
+++ tests/events.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Bauwoche 2024 screenshot', async ({ page, isMobile }) => {
+	await page.goto('/events/bauwoche-2024');
+	if (!isMobile) {
+		// load lazy loaded image
+		await page.getByRole('img', { name: 'Wochenplan Bauwoche 2024' }).scrollIntoViewIfNeeded();
+	}
+	await page.getByRole('contentinfo').scrollIntoViewIfNeeded();
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
