f3d67ee7ca4262e1e803f835692a203cef297d84
Jonas Bulik <jonas@bulik.dev>
2024-02-28 09:46:10 +0100

    test(app): 33 expanding exercises

--- tests/about-us.spec.ts
+++ tests/about-us.spec.ts
@@ -21,3 +21,10 @@ test('About Us page screenshot', async ({ page }) => {
   await forceLoadImages(page);
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
+
+test('Exercises can be expanded', async ({ page }) => {
+	await expect(page.getByRole('heading', { name: 'Tanzen' })).not.toBeVisible();
+	await page.getByRole('button', { name: 'Mehr lesen' }).click();
+	await expect(page.getByRole('heading', { name: 'Tanzen' })).toBeVisible();
+	await page.getByRole('button', { name: 'Weniger' }).click();
+});
