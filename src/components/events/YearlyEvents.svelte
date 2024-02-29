138fe9c734f0984454c880345e2de899905d7a56
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-02-29 17:52:14 +0100

    feat(app): update filter logic for yearly events (#51)
    Update filter logic for yearly events so it shows either events for all or only for members.

--- src/components/events/YearlyEvents.svelte
+++ src/components/events/YearlyEvents.svelte
@@ -4,8 +4,13 @@
 
 	export let events: YVEvent[];
 
-	let for_all = true;
-	let only_members = true;
+	let filter_for_all = true;
+
+	function toggleForAll() {
+		filter_for_all = !filter_for_all;
+	}
+
+	$: filteredEvents = events.filter((e) => e.for_all === filter_for_all);
 </script>
 
 <section class="relative mb-20 px-4 pt-14 lg:px-20 lg:pt-24">
@@ -19,46 +24,56 @@
 	</p>
 	<div class="mt-3 flex flex-row">
 		<div class="flex items-center">
-			<input class="h-7 w-7" type="checkbox" id="for-all" bind:checked={for_all} />
+			<input
+				class="h-7 w-7"
+				type="checkbox"
+				id="for-all"
+				on:click={toggleForAll}
+				checked={filter_for_all}
+			/>
 			<label class="ml-4" for="for-all">Für Alle</label>
 		</div>
 		<div class="ml-10 flex items-center lg:ml-28">
-			<input class="h-7 w-7" type="checkbox" id="only-members" bind:checked={only_members} />
+			<input
+				class="h-7 w-7"
+				type="checkbox"
+				id="only-members"
+				on:click={toggleForAll}
+				checked={!filter_for_all}
+			/>
 			<label class="ml-4" for="only-members">Nur Mitglieder</label>
 		</div>
 	</div>
 	<ul class="flex flex-col lg:mt-12 lg:grid lg:grid-cols-2 lg:gap-10">
-		{#each events as event}
-			{#if (event.for_all && for_all) || (!event.for_all && only_members)}
-				<li
-					class="mt-12 rounded-2xl shadow-lg lg:mt-0 {event.for_all
-						? 'bg-yellow-700'
-						: 'bg-light-green'}"
-				>
-					<img
-						alt=""
-						src={event.image.src.src}
-						{...event.image.src.attributes}
-						class="max-h-64 w-full rounded-t-2xl object-cover"
-					/>
-					<div class="flex p-6">
-						<div class="mt-4 flex flex-col items-center">
-							<div class="font-serif text-5xl font-bold">{event.day}</div>
-							<div>{event.month}</div>
-						</div>
-						<div class="ml-10">
-							<h3 class="inline-block font-bold lg:text-xl">{event.title}</h3>
-							{#if event.future}
-								<span class="italic">ab {event.future}</span>
-							{/if}
-							<p class="mt-3">{event.short_description}</p>
-							<p class="mt-3 inline-block rounded-full bg-white px-5">
-								{event.for_all ? 'Für Alle' : 'Nur Mitglieder'}
-							</p>
-						</div>
+		{#each filteredEvents as event}
+			<li
+				class="mt-12 rounded-2xl shadow-lg lg:mt-0 {event.for_all
+					? 'bg-yellow-700'
+					: 'bg-light-green'}"
+			>
+				<img
+					alt=""
+					src={event.image.src.src}
+					{...event.image.src.attributes}
+					class="max-h-64 w-full rounded-t-2xl object-cover"
+				/>
+				<div class="flex p-6">
+					<div class="mt-4 flex flex-col items-center">
+						<div class="font-serif text-5xl font-bold">{event.day}</div>
+						<div>{event.month}</div>
 					</div>
-				</li>
-			{/if}
+					<div class="ml-10">
+						<h3 class="inline-block font-bold lg:text-xl">{event.title}</h3>
+						{#if event.future}
+							<span class="italic">ab {event.future}</span>
+						{/if}
+						<p class="mt-3">{event.short_description}</p>
+						<p class="mt-3 inline-block rounded-full bg-white px-5">
+							{event.for_all ? 'Für Alle' : 'Nur Mitglieder'}
+						</p>
+					</div>
+				</div>
+			</li>
 		{/each}
 	</ul>
 </section>
