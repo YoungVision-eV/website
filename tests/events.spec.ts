9d8ed2c23036d183fb1c70483de363e5724480b9
Jonas <jonas@bulik.dev>
2024-03-05 14:05:25 +0100

    update bauwochen assets (#86)
    * chore(website): fix small typos in bauwochen plan
    
    * chore(website): update screenshots

--- tests/events.spec.ts
+++ tests/events.spec.ts
@@ -1,11 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('Bauwoche 2024 screenshot', async ({ page, isMobile }) => {
+test('Bauwoche 2024 screenshot', async ({ page }) => {
   await page.goto('/events/bauwoche-2024');
-  if (!isMobile) {
-    // load lazy loaded image
-    await page.getByRole('img', { name: 'Wochenplan Bauwoche 2024' }).scrollIntoViewIfNeeded();
-  }
   await page.getByRole('contentinfo').scrollIntoViewIfNeeded();
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
