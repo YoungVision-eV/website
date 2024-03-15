f3504d1738d453f7e24f3133c531e7f71cd3c0e9
Jonas <jonas@bulik.dev>
2024-03-15 12:20:34 +0100

    feat(app): #38 update posts from instagram API (#78)
    * feat(app): #38 update posts from instagram API
    
    This might break at any time if instagram changes the query_id. But the alternative is setting up an OAuth app which I couldn't get to work and might not be worth the hassle (at the moment).
    
    Closes: #38
    
    * test(website): #38 use hard coded insta posts in tests
    
    * perf(website): #33 Correct Image optimizations for Instagram posts
    
    Because of the h-52 w-52 clases on the Image component we know the image will always be 13rem wide. We now use this information in the sizes attribute and optmize the images accordingly.
    
    * refactor(website): #38 Make count of insta posts easy to change
    
    Just added a count argument to the getRecentPhotos function.

--- src/data/instagram.ts
+++ src/data/instagram.ts
@@ -7,8 +7,8 @@ import fourthPost from '@assets/instagram/fourth-post.jpeg';
 import secondPost from '@assets/instagram/second-post.png';
 import thirdPost from '@assets/instagram/third-post.png';
 
-async function getPhoto(src: ImageMetadata) {
-  return getImage({ src, widths: [96, 192, 256, 512] });
+async function getPhoto(src: string | ImageMetadata) {
+  return getImage({ src, width: 208, height: 208, densities: [1, 1.5, 2] });
 }
 
 type InstagramPost = {
@@ -17,32 +17,75 @@ type InstagramPost = {
   link: string;
 };
 
-export async function getRecentPhotos(): Promise<InstagramPost[]> {
-  return [
+type InstagramAPINode = {
+  node: {
+    display_url: string;
+    dimensions: {
+      height: number;
+      width: number;
+    };
+    edge_media_to_caption: {
+      edges: {
+        node: {
+          text: string;
+        };
+      }[];
+    };
+    shortcode: string;
+  };
+};
+
+export async function getRecentPhotos(count: number): Promise<InstagramPost[]> {
+  if (process.env.PLAYWRIGHT_TEST === 'true') {
+    const fakePosts = [
+      {
+        src: await getPhoto(firstPost),
+        alt: 'First Post',
+        link: 'https://www.instagram.com/youngvision_ev/',
+      },
+      {
+        src: await getPhoto(secondPost),
+        alt: 'Second Post',
+        link: 'https://www.instagram.com/youngvision_ev/',
+      },
+      {
+        src: await getPhoto(thirdPost),
+        alt: 'Third Post',
+        link: 'https://www.instagram.com/youngvision_ev/',
+      },
+
+      {
+        src: await getPhoto(fourthPost),
+        alt: 'Fourth Post',
+        link: 'https://www.instagram.com/youngvision_ev/',
+      },
+      {
+        src: await getPhoto(fifthPost),
+        alt: 'Fifth Post',
+        link: 'https://www.instagram.com/youngvision_ev/',
+      },
+    ];
+    // Just repeat fake posts to get the desired count
+    return Array(count)
+      .fill(null)
+      .map((_, i) => fakePosts[i % fakePosts.length]);
+  }
+  const { data } = await fetch(
+    `https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables={%22id%22:%223938579639%22,%22first%22:${count},%22after%22:null}`,
     {
-      src: await getPhoto(firstPost),
-      alt: 'First Post',
-      link: 'https://www.instagram.com/youngvision_ev/',
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
     },
-    {
-      src: await getPhoto(secondPost),
-      alt: 'Second Post',
-      link: 'https://www.instagram.com/youngvision_ev/',
-    },
-    {
-      src: await getPhoto(thirdPost),
-      alt: 'Third Post',
-      link: 'https://www.instagram.com/youngvision_ev/',
-    },
-    {
-      src: await getPhoto(fourthPost),
-      alt: 'Fourth Post',
-      link: 'https://www.instagram.com/youngvision_ev/',
-    },
-    {
-      src: await getPhoto(fifthPost),
-      alt: 'Fifth Post',
-      link: 'https://www.instagram.com/youngvision_ev/',
-    },
-  ];
+  ).then((res) => res.json());
+  const posts = data.user.edge_owner_to_timeline_media.edges as InstagramAPINode[];
+
+  return Promise.all(
+    posts.map(async ({ node: p }: InstagramAPINode) => ({
+      src: await getPhoto(p.display_url),
+      alt: p.edge_media_to_caption.edges[0].node.text!,
+      link: `https://instagram.com/p/${p.shortcode}`,
+    })),
+  );
 }
