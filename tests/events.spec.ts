6060ea6f886d9478d5e558aa47737647f2c1cb2b
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-06-13 12:18:15 +0200

    test(website): use forceLoadImages, update Snapshots

--- tests/events.spec.ts
+++ tests/events.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@playwright/test';
+import { forceLoadImages } from './fixtures';
 
 test('Bauwoche 2024 screenshot', async ({ page }) => {
   await page.goto('/events/bauwoche-2024');
@@ -8,6 +9,7 @@ test('Bauwoche 2024 screenshot', async ({ page }) => {
 
 test('Event Page screenshot', async ({ page }) => {
   await page.goto('/events/event-1');
+  await forceLoadImages(page);
   await page.getByRole('contentinfo').scrollIntoViewIfNeeded();
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
