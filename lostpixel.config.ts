649ac425170c73d84e3bcfa23c9aac8cba727b68
Jonas Bulik <jonas@bulik.dev>
2024-07-04 11:52:00 +0200

    ci(tools): #21 fix port

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,7 +3,7 @@ import type { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/', name: 'landing' }],
-    baseUrl: 'http://172.17.0.1:3000',
+    baseUrl: 'http://172.17.0.1:4321',
   },
   lostPixelProjectId: 'clrkf72110ugom40el0d2rkx9',
   apiKey: process.env.LOST_PIXEL_API_KEY,
