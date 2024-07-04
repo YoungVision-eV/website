b42837b8509d0775074a5dc9d22af2e0f5598af6
Jonas Bulik <jonas@bulik.dev>
2024-07-04 15:19:43 +0200

    test(tools): #21 add all pages for lost-pixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,15 @@ import type { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ path: '/', name: 'landing' }],
+    pages: [
+      { path: '/', name: 'Home' },
+      { path: '/about-us', name: 'Über Uns' },
+      { path: '/events', name: 'Veranstaltungen' },
+      { path: '/events/bauwoche-2024', name: 'Bauwoche 2024' },
+      { path: '/events/event-1', name: 'Event 1' },
+      { path: '/support-us', name: 'Unterstütze uns' },
+      { path: '/members', name: 'Mitgliedschaft' },
+    ],
     baseUrl: 'http://172.17.0.1:4321',
   },
   lostPixelProjectId: 'clrkf72110ugom40el0d2rkx9',
