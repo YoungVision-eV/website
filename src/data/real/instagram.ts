9334bb358db0375ec5e6eae346958703f5f7010e
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-07-13 00:56:30 +0200

    refactor(website): split insta real and test data

--- src/data/real/instagram.ts
+++ src/data/real/instagram.ts
@@ -1,20 +1,13 @@
-import type { GetImageResult, ImageMetadata } from 'astro';
+import type { ImageMetadata } from 'astro';
 import { getImage } from 'astro:assets';
-
-import fifthPost from '@assets/instagram/fifth-post.png';
-import firstPost from '@assets/instagram/first-post.png';
-import fourthPost from '@assets/instagram/fourth-post.jpeg';
-import secondPost from '@assets/instagram/second-post.png';
-import thirdPost from '@assets/instagram/third-post.png';
+import type { InstagramPost } from '../index.ts';
 
 async function getPhoto(src: string | ImageMetadata) {
   return getImage({ src, width: 208, height: 208, densities: [1, 1.5, 2] });
 }
 
-type InstagramPost = {
-  src: GetImageResult;
-  alt: string;
-  link: string;
+export const realInstagramData = {
+  getRecentPhotos,
 };
 
 type InstagramAPINode = {
@@ -36,45 +29,6 @@ type InstagramAPINode = {
 };
 
 export async function getRecentPhotos(count: number): Promise<InstagramPost[]> {
-  // Use fake posts in dev because otherwise we hit instagrams rate limit very quickly
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.PLAYWRIGHT_TEST === 'true' ||
-    process.env.PREVIEW_DEPLOYMENT === 'true'
-  ) {
-    const fakePosts = [
-      {
-        src: await getPhoto(firstPost),
-        alt: 'First Post',
-        link: 'https://www.instagram.com/youngvision_ev/',
-      },
-      {
-        src: await getPhoto(secondPost),
-        alt: 'Second Post',
-        link: 'https://www.instagram.com/youngvision_ev/',
-      },
-      {
-        src: await getPhoto(thirdPost),
-        alt: 'Third Post',
-        link: 'https://www.instagram.com/youngvision_ev/',
-      },
-
-      {
-        src: await getPhoto(fourthPost),
-        alt: 'Fourth Post',
-        link: 'https://www.instagram.com/youngvision_ev/',
-      },
-      {
-        src: await getPhoto(fifthPost),
-        alt: 'Fifth Post',
-        link: 'https://www.instagram.com/youngvision_ev/',
-      },
-    ];
-    // Just repeat fake posts to get the desired count
-    return Array(count)
-      .fill(null)
-      .map((_, i) => fakePosts[i % fakePosts.length]);
-  }
   const response = await fetch(
     `https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables={%22id%22:%223938579639%22,%22first%22:${count},%22after%22:null}`,
     {
