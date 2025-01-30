4ea47ce17a7d48ed4e8eed23a91b7993d61d971f
Jonas Bulik <jonas@bulik.dev>
2025-01-30 12:11:18 +0100

    refactor(website): #87 use melt-ui for fördermitglieder
    This is preparation for the update to svelte 5.
    
    Closes: #87

--- src/components/support-us/Disclosure.svelte
+++ src/components/support-us/Disclosure.svelte
@@ -1,6 +1,10 @@
 <script>
   import { slide, fade } from 'svelte/transition';
-  import { Disclosure, DisclosureButton, DisclosurePanel } from '@rgossiaux/svelte-headlessui';
+  import { createCollapsible, melt } from '@melt-ui/svelte';
+  const {
+    elements: { root, content, trigger },
+    states: { open },
+  } = createCollapsible();
 
   const benefits = [
     'Unsere Arbeit langfristig ermöglichen',
@@ -11,47 +15,48 @@
   ];
 </script>
 
-<Disclosure data-testid="benefits" let:open>
-  {#if open}
-    <div transition:slide>
-      <DisclosurePanel class="pb-12 pt-16 lg:px-16" static>
-        <h3 class="font-serif text-2xl font-bold">Vorteile</h3>
-        <ol id="benefits" class="grid grid-cols-3 py-12 lg:gap-y-14">
-          {#each benefits as benefit, index}
-            <li class="group col-span-3 grid grid-cols-subgrid items-center">
-              <div
-                class="flex flex-none flex-row group-odd:order-last group-odd:justify-end
+<div use:melt={$root} data-testid="benefits">
+  {#if $open}
+    <div transition:slide use:melt={$content} class="pb-12 pt-16 lg:px-16">
+      <h3 class="font-serif text-2xl font-bold">Vorteile</h3>
+      <ol id="benefits" class="grid grid-cols-3 py-12 lg:gap-y-14">
+        {#each benefits as benefit, index}
+          <li class="group col-span-3 grid grid-cols-subgrid items-center">
+            <div
+              class="flex flex-none flex-row group-odd:order-last group-odd:justify-end
 									lg:group-odd:order-first lg:group-odd:justify-start"
-              >
-                <div
-                  class="flex h-24 w-24 flex-none items-center justify-center rounded-full
+            >
+              <div
+                class="flex h-24 w-24 flex-none items-center justify-center rounded-full
 										bg-gray-300 font-serif text-5xl font-bold lg:h-28 lg:w-28 lg:bg-green-50 lg:text-white"
-                >
-                  {index + 1}
-                </div>
+              >
+                {index + 1}
               </div>
-              <p class="col-span-2 ml-2 text-sm lg:ml-8 lg:mr-5 lg:text-xl">
-                {benefit}
-              </p>
-            </li>
-          {/each}
-        </ol>
-        <div class="grid grid-cols-1 gap-y-4 lg:grid-cols-2 lg:gap-x-5 lg:text-xl">
-          <p>
-            Dein Beitrag, unsere Vision: Deine Unterstützung ermöglicht es uns, inspirierende
-            Projekte und Veranstaltungen für junge Menschen zu schaffen und eine unterstützende
-            Gemeinschaft aufzubauen.
-          </p>
-          <p>
-            Wir schätzen deine Beteiligung und freuen uns darauf, gemeinsam eine bessere Zukunft zu
-            gestalten.
-          </p>
-        </div>
-      </DisclosurePanel>
+            </div>
+            <p class="col-span-2 ml-2 text-sm lg:ml-8 lg:mr-5 lg:text-xl">
+              {benefit}
+            </p>
+          </li>
+        {/each}
+      </ol>
+      <div class="grid grid-cols-1 gap-y-4 lg:grid-cols-2 lg:gap-x-5 lg:text-xl">
+        <p>
+          Dein Beitrag, unsere Vision: Deine Unterstützung ermöglicht es uns, inspirierende Projekte
+          und Veranstaltungen für junge Menschen zu schaffen und eine unterstützende Gemeinschaft
+          aufzubauen.
+        </p>
+        <p>
+          Wir schätzen deine Beteiligung und freuen uns darauf, gemeinsam eine bessere Zukunft zu
+          gestalten.
+        </p>
+      </div>
     </div>
   {/if}
-  <DisclosureButton class="mt-12 flex w-full flex-col items-center justify-center lg:mt-8">
-    <span class="text-sm">{open ? 'Weniger' : 'Mehr'}</span>
+  <button
+    use:melt={$trigger}
+    class="mt-12 flex w-full flex-col items-center justify-center lg:mt-8"
+  >
+    <span class="text-sm">{$open ? 'Weniger' : 'Mehr'}</span>
     <div>
       <svg
         class="mt-2"
@@ -61,7 +66,7 @@
         viewBox="0 0 13 9"
         fill="none"
       >
-        {#if open}
+        {#if $open}
           <path
             transition:fade
             fill-rule="evenodd"
@@ -80,8 +85,8 @@
         {/if}
       </svg>
     </div>
-  </DisclosureButton>
-</Disclosure>
+  </button>
+</div>
 
 <style>
   @media (min-width: 1024px) {
