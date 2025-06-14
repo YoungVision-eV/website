9ef9be43ff9ad1ec749269261de27cb0e4d89c1e
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-06-14 12:49:43 +0200

    fix(website): fix Calendar build error by removing Tween

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -3,7 +3,6 @@
 
   import clsx from 'clsx';
   import { cubicOut } from 'svelte/easing';
-  import { Tween } from 'svelte/motion';
   import { blur, fade } from 'svelte/transition';
 
   export let events: [EventCalendarEntry, EventCalendarEntry, EventCalendarEntry];
@@ -24,8 +23,6 @@
 
   const ANIMATION_DURATION = 300;
 
-  const location = new Tween(selectedEvent, { duration: ANIMATION_DURATION, easing: cubicOut });
-  $: location.set(selectedEvent);
   $: currentEvent = events[selectedEvent];
 </script>
 
@@ -59,7 +56,7 @@
 				-->
         <svg
           class="absolute h-5 w-5 text-white"
-          style="top: 0%; transform: translateY(calc({location.current * 100}% + {location.current *
+          style="top: 0%; transform: translateY(calc({selectedEvent * 100}% + {selectedEvent *
             0.25}rem))"
           fill="currentColor"
           viewBox="0 0 100 100"
@@ -95,7 +92,7 @@
         'bg-background absolute z-20 h-1/3 w-full lg:-ml-8 lg:w-[calc(100%+2rem)] lg:rounded-l-2xl',
         selectedEvent === 0 ? 'rounded-t-2xl' : '',
       )}
-      style="top: 0%; transform: translateY({location.current * 100}%)"
+      style="top: 0%; transform: translateY({selectedEvent * 100}%)"
     ></div>
     {#each events as event, index (event.title)}
       <li
