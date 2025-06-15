b91d5780fbb34385ac78e8b58d10a6d41a6345bb
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-06-15 16:34:45 +0200

    feat(website): check for missing fields

--- src/pages/api/support-us.ts
+++ src/pages/api/support-us.ts
@@ -3,7 +3,18 @@ import type { APIRoute } from 'astro';
 
 import { gql, GraphQLClient } from 'graphql-request';
 export const POST: APIRoute = async ({ request }) => {
-  // TODO: use env variable for url to twenty graphql
+  const requiredFields = [
+    'first-name',
+    'last-name',
+    'email',
+    'street',
+    'city',
+    'post-code',
+    'country',
+    'iban',
+    'amount',
+  ];
+
   const graphQLClient = new GraphQLClient(import.meta.env.TWENTY_GRAPHQL_URL, {
     headers: {
       Authorization: 'Bearer ' + import.meta.env.TWENTY_AUTH,
@@ -12,29 +23,61 @@ export const POST: APIRoute = async ({ request }) => {
   });
   try {
     const data = await request.formData();
+
+    // Check if any required fields are missing
+    const missingFields = requiredFields.filter((field) => data.get(field) === null);
+    if (missingFields.length > 0) {
+      return new Response(
+        JSON.stringify({
+          fields: missingFields,
+          message: 'Missing required field(s)',
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 400,
+        },
+      );
+    }
+
     const person = {
       adresse: { addressCity: 'a', addressPostcode: 'b', addressState: 'c', addressStreet1: 'd' },
       emails: { primaryEmail: data.get('email') },
       name: { firstName: data.get('first-name'), lastName: data.get('last-name') },
     };
+    const bic = data.get('bic');
+    const personData = {
+      adresse: {
+        addressCity: data.get('city'),
+        addressCountry: data.get('country'),
+        addressPostcode: data.get('post-code'),
+        addressStreet1: data.get('street'),
+      },
+      emails: { primaryEmail: data.get('email') },
+      name: { firstName: data.get('first-name'), lastName: data.get('last-name') },
+    };
+
+    const fordermitgliedschaftData = {
+      fullName: { firstName: data.get('first-name'), lastName: data.get('last-name') },
+      iban: data.get('iban'),
+      monatlicherForderbeitrag: {
+        amountMicros: Number(data.get('amount') ?? 0) * 1000000,
+        currencyCode: 'EUR',
+      },
+      ...(bic ? { bic } : {}),
+      beitrittsdatum: new Date().toJSON(),
+    };
+
+    // With the replace method the the quotes from the property keys (from JSON) are removed, because graphql requires them unquoted
     const mutation = gql`
       mutation {
-        createPerson(data: {
-          adresse: { addressCity: "${data.get('city')}", addressPostcode: "${data.get('post-code')}", addressCountry: "${data.get('country')}", addressStreet1: "${data.get('street')}" },
-          emails: { primaryEmail: "${data.get('email')}" },
-          name: { firstName: "${data.get('first-name')}", lastName: "${data.get('last-name')}" },
-        }) {
+        createPerson(data: ${JSON.stringify(personData).replace(/"([^"]+)":/g, '$1:')}) {
           id
           createdAt
         }
         createFordermitgliedschaft(
-          data: {
-            fullName: { firstName: "${data.get('first-name')}", lastName: "${data.get('last-name')}" },
-            monatlicherForderbeitrag: { amountMicros: ${Number(data.get('amount') ?? 0) * 1000000}, currencyCode: "EUR" },
-            iban: "${data.get('iban')}",
-            bic: "${data.get('bic')}",
-            beitrittsdatum: "${new Date().toJSON()}",
-          }
+          data: ${JSON.stringify(fordermitgliedschaftData).replace(/"([^"]+)":/g, '$1:')}
         ) {
           id
           monatlicherForderbeitrag {
@@ -44,6 +87,7 @@ export const POST: APIRoute = async ({ request }) => {
         }
       }
     `;
+    console.log(mutation);
     //TODO: link the Fordermitgliedschaft with the person
     interface ResponseData {
       createPerson: {
@@ -57,6 +101,12 @@ export const POST: APIRoute = async ({ request }) => {
     if (error instanceof Error) {
       console.error(error.message);
     }
+    return new Response(
+      JSON.stringify({
+        message: 'error',
+      }),
+      { status: 500 },
+    );
   }
   // TODO: return failure on failure
   return new Response(
